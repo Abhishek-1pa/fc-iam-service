6b5b1306c81b5c3f433e85a98a9252c522ad9892
frozenmafia <shivajay295@gmail.com>
2024-01-21 20:08:19 +0530

    Update code

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,18 @@
+#!/bin/bash
+
+# Push code to GitHub
 git add .
-git commit -m "update code"
+git commit -m "Update code"
 git push origin main
 
-docker build -t iam-service .
+# Build and push Docker image to Docker Hub
+docker build -t anchalshivank/iam-service .
 docker push anchalshivank/iam-service
 
-ssh -i projectuser projectuser@34.131.181.137 << 'EOF'
-sudo docker pull anchalshivank/fc-iam-service
+# SSH into your VM and redeploy the Docker container
+ssh -i /path/to/your/private/key projectuser@34.131.181.137 << 'EOF'
+sudo docker pull anchalshivank/iam-service
 sudo docker stop iam-container
 sudo docker rm iam-container
-sudo docker run -d -p 8000:8000 --name iam-container anchalshivank/fc-iam-service
+sudo docker run -d -p 8000:8000 --name iam-container anchalshivank/iam-service
+EOF
