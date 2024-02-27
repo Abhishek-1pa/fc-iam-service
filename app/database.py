f29911c3a0d8b444e1e1d2bcb02a70652d8b3fe0
frozenmafia <shivajay295@gmail.com>
2024-02-28 03:08:16 +0530

    database reset

--- app/database.py
+++ app/database.py
@@ -2,10 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-# SQLALCHEMY_DATABASE_URL_PROD = 'postgresql://postgres:123qwe123qwe@35.238.209.92/postgres'
+SQLALCHEMY_DATABASE_URL_PROD = 'postgresql://postgres:123qwe123qwe@35.238.209.92/postgres'
 SQLALCHEMY_DATABASE_URL_DEV = 'postgresql://postgres:123qwe123qwe@localhost/forgecode'
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL_DEV
+    SQLALCHEMY_DATABASE_URL_PROD
 )
 
 SessionLocal = sessionmaker(autocommit = False, autoflush=False,  bind=engine)
