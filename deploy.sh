bc607b2f21a91602fcf3453a5a36fd9b9d32a5b6
frozenmafia <shivajay295@gmail.com>
2024-03-01 23:55:01 +0530

    t

--- deploy.sh
+++ deploy.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
-
 # Change directory to 'backend' directory
-if [! -d "backend"]; then
-    mkdir backened
+if [ ! -d "backend" ]; then
+    mkdir backend
 fi
 
-cd backened
+cd backend
 
 # Check if the repository exists, if not, clone it
-if [ ! -d "fc-iam-service" ]; then
-    git clone https://github.com/frozenmafia/fc-iam-service.git
+if [ ! -d "fc-blog-service" ]; then
+    git clone https://github.com/frozenmafia/fc-blog-service.git
 fi
 
 # Change directory to the cloned repository
-cd fc-iam-service
+cd fc-blog-service
 
 # Pull the latest changes from the repository
 git pull
 
-# Build the new Docker image for fc-iam-service
-sudo docker build -t fc-iam-service-new .
+# Build the new Docker image for fc-blog-service
+sudo docker build -t fc-blog-service-new .
 
 # Check if the container exists
-if sudo docker ps -a --format '{{.Names}}' | grep -Eq '^fc-iam-service$'; then
+if sudo docker ps -a --format '{{.Names}}' | grep -Eq '^fc-blog-service$'; then
     # Stop the existing container
-    sudo docker stop fc-iam-service
+    sudo docker stop fc-blog-service
 
     # Remove the existing container
-    sudo docker rm fc-iam-service
+    sudo docker rm fc-blog-service
 fi
 
-# Run the new Docker container for fc-iam-service with restart options
-sudo docker run -d --restart=always --name fc-iam-service -p 8001:8001 fc-iam-service-new
+# Run the new Docker container for fc-blog-service with restart options
+sudo docker run -d --restart=always --name fc-blog-service -p 8002:8002 fc-blog-service-new
